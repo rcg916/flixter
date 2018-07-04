580934f8f4742d4ed35eaf50f39988ba9949710e
Randy Goss <randycgoss@gmail.com>
2018-07-04 06:50:56 +0000

    Added ability to properly process CC vis Stripe, and show buttons according to enrollment status

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   has_many :sections
   has_many :enrollments
@@ -7,5 +9,11 @@ class Course < ApplicationRecord
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 
-  mount_uploader :image, ImageUploader
+  def free?
+    cost.zero?
+  end
+
+  def premium?
+    ! free?
+  end
 end
